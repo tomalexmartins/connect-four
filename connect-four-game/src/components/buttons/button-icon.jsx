63fc78c2dd93e8512e6c31c88c1aa8faf4fcbb08
Tomás Martins <martinscampostomas@gmail.com>
2025-05-15 17:08:35 +0100

    Create Pages and Router (#2)
    Co-authored-by: Tomás Martins <>

---
+++ connect-four-game/src/components/buttons/button-icon.jsx
@@ -0,0 +1,8 @@
+//EXEMPLO MUDAR
+function IconButton() {
+  return (
+    <button className="mt-6 bg-yellow text-navy px-4 py-2 rounded shadow-lg hover:bg-redish hover:text-white transition flex items-center gap-2"></button>
+  );
+}
+
+export default IconButton;

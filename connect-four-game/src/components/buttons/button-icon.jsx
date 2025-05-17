05a07e2acf3b55ab1997d873280104f2c5ff13bd
Matilde <matildepinto914@gmail.com>
2025-05-17 15:50:22 +0100

    iconbutton

--- connect-four-game/src/components/buttons/button-icon.jsx
+++ connect-four-game/src/components/buttons/button-icon.jsx
@@ -1,8 +1,21 @@
-//EXEMPLO MUDAR
-function IconButton() {
-  return (
-    <button className="mt-6 bg-yellow text-navy px-4 py-2 rounded shadow-lg hover:bg-redish hover:text-white transition flex items-center gap-2"></button>
-  );
-}
+
+
+/**
+ * Reusable menu button
+ */
+import React from "react";
+
+const IconButton = ({ label, className="", width = 434, height = 83 }) => (
+  <button
+    className={`flex items-center px-6 bg-white text-black border-3 border-black rounded-2xl hover:bg-gray-100 transition font-[Pixelilfy-sans] font-bold ${className}`}
+    style={{
+      width: `${width}px`,
+      height: `${height}px`,
+    }}
+  >
+    
+    {label}
+  </button>
+);
 
 export default IconButton;

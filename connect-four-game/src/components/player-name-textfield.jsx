af5bad44baa450b24c68a3112ee2fd1ce5f29d98
Tomás Martins <>
2025-05-17 16:29:40 +0100

    [WIP]

---
+++ connect-four-game/src/components/player-name-textfield.jsx
@@ -0,0 +1,18 @@
+function PlayerNameTf({ playerNumber, value, onChange, color }) {
+  return (
+    <div className="flex flex-col space-y-[16px]">
+      <p className="font-[Pixel] font-normal leading-[1.1] text-[32px]">
+        Player {playerNumber}
+      </p>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={`insert player ${playerNumber} name`}
+        className={`bg-${color} text-black placeholder-black font-[Pixel] font-normal tracking-[1px] leading-[1.1] w-[500px] h-[83px] border-[5px] pl-[16px]`}
+      />
+    </div>
+  );
+}
+
+export default PlayerNameTf;

6d7c43e55973e0b42b6b176f4ae42ce24a3db775
Matilde <matildepinto914@gmail.com>
2025-05-21 11:28:36 +0100

    [DONE] Homepage

--- connect-four-game/src/components/buttons/button-icon.jsx
+++ connect-four-game/src/components/buttons/button-icon.jsx
@@ -5,16 +5,16 @@
  */
 import React from "react";
 
-const IconButton = ({ label, icon, className="", width = 434, height = 83,  }) => (
-  <button
-    className={`flex items-center px-6 bg-whiteParchment text-black border-5 border-black  hover:bg-gray-100 transition font-[Pixel] gap-2 ${className}`}
+const IconButton = ({ label, icon, className="", width = 434, height = 83, onClick }) => (
+  <button onClick={onClick}
+    className={`flex items-center px-6 bg-whiteParchment text-black border-5 border-black  cursor-pointer hover:opacity-50 transition-opacity duration-200 font-[Pixel] gap-2 ${className}`}
     style={{
       width: `${width}px`,
       height: `${height}px`,
     }}
-  >{label}
+  ><span className="whitespace-nowrap overflow-hidden text-ellipsis ">{label}</span>
     {icon && (
-      <div className="flex items-center gap-1">
+      <div className="flex items-center ">
         {icon.includes( "ic_user.svg" )? (
           <>
             <img src={icon} alt="icon" />

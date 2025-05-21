acacd9501e3816d123a1fd580b61e54be356b666
matilde178 <135722140+matilde178@users.noreply.github.com>
2025-05-21 11:47:18 +0100

    Home page (#6)
    * iconbutton
    
    * iconbutton
    
    * [WIP] HomePage
    
    * [WIP] HomePage
    
    * [DONE] Homepage
    
    * iconbutton
    
    * iconbutton
    
    * [WIP] HomePage
    
    * [DONE] Homepage

--- connect-four-game/src/components/buttons/button-icon.jsx
+++ connect-four-game/src/components/buttons/button-icon.jsx
@@ -5,16 +5,26 @@
  */
 import React from "react";
 
-const IconButton = ({ label, className="", width = 434, height = 83 }) => (
-  <button
-    className={`flex items-center px-6 bg-white text-black border-3 border-black rounded-2xl hover:bg-gray-100 transition font-[Pixelilfy-sans] font-bold ${className}`}
+const IconButton = ({ label, icon, className="", width = 434, height = 83, onClick }) => (
+  <button onClick={onClick}
+    className={`flex items-center px-6 bg-whiteParchment text-black border-5 border-black  cursor-pointer hover:opacity-50 transition-opacity duration-200 font-[Pixel] gap-2 ${className}`}
     style={{
       width: `${width}px`,
       height: `${height}px`,
     }}
-  >
-    
-    {label}
+  ><span className="whitespace-nowrap overflow-hidden text-ellipsis ">{label}</span>
+    {icon && (
+      <div className="flex items-center ">
+        {icon.includes( "ic_user.svg" )? (
+          <>
+            <img src={icon} alt="icon" />
+            <img src={icon} alt="icon" />
+          </>
+        ) : (
+          <img src={icon} alt="icon" />
+        )}
+      </div>
+    )}
   </button>
 );
 
--- connect-four-game/src/pages/home-page.jsx
+++ connect-four-game/src/pages/home-page.jsx
@@ -1,21 +1,42 @@
 import IconButton from "../components/buttons/button-icon";
+import GameTitle from "../components/game-title";
+import Info from "/assets/icons/ic_info.svg";
+import User from "/assets/icons/ic_user.svg"
+import Users from "/assets/icons/ic_users.svg"
+import { Link } from "react-router-dom";
+import StartGameModal from "../components/modal/start-game-modal";
+import { useState } from "react";
+
 
 const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+
   return (
     <div className="flex items-center justify-center h-screen gap-[220px]">
         {/* Coluna da Esquerda */}
-        <div className="pl-[268px] flex flex-col text-[30px]">
-          
-          <IconButton label="how to play?" className="text-[30px] " 
+        <div className="pl-[268px] flex flex-col justify-center items-center  gap-[70px] ">
+          <GameTitle textSize={"text-[100px]"} stokeWeight={"15px"}></GameTitle>
+          <Link to="/rules">
+          <IconButton label="how to play?" icon={Info} className="text-[25px] " 
           width={250}
           height={70} />
+          </Link>
         </div>
 
         {/* Coluna da Direita */}
         <div className="flex flex-col items-center gap-[42px] pr-[140px]">
-          <IconButton label="player vs player" className="text-[32px] " />
-          <IconButton label="player vs computer"className="text-[32px] "  />
+          <IconButton label="player vs player" icon={User} className="text-[32px] " onClick={() =>setShowModal(true) } />
+          <IconButton label="player vs computer" icon={Users} className="text-[32px] " onClick={() =>setShowModal (true) } />
         </div>
+      
+        
+        {showModal && (
+          
+  <div className="fixed inset-0 z-50 w-full flex justify-center mt-[100px] mb-[100px]">     
+        <StartGameModal onClose={() => setShowModal(false)} /></div>
+      )}
+        
       
     </div>
   );

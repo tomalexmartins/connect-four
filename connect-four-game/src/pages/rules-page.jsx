9a163718b92db49ed04bfea32b622108b7402ccf
sellz-it <selina.mgllz@gmail.com>
2025-06-01 00:25:06 +0100

    player card e rules page

--- connect-four-game/src/pages/rules-page.jsx
+++ connect-four-game/src/pages/rules-page.jsx
@@ -1,7 +1,18 @@
+import RulesBox from "../components/modal/rulesbox";
+import GameTitle from "../components/game-title";
+
 function RulesPage() {
   return (
-    <div className="min-h-screen bg-cream text-navy font-pixel flex flex-col items-center justify-center">
-      <h1 className="text-5xl text-redish">RULES PAGE</h1>
+    <div className="min-h-screen bg-blueRoyal">
+      {/* Cabeçalho com o título no canto superior esquerdo */}
+      <div className="flex justify-between items-start pt-[50px] px-[32px]">
+        <GameTitle textSize="text-[48px]" stokeWeight="10px" />
+      </div>
+
+      {/* Caixa de regras centralizada, bem mais acima */}
+      <div className="flex justify-center mt-0  overflow-y-auto">
+        <RulesBox />
+      </div>
     </div>
   );
 }
